200ee7451668e3f4b594b90a8bfb4f2230f960a2
Matthew Little <zone117x@gmail.com>
2022-09-27 11:06:43 +0200

    chore: simplify compose profile setup

--- snapshot-init.sh
+++ snapshot-init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker compose down --volumes --remove-orphans --timeout=1 --rmi=all
-docker compose --profile snapshot-init up --build
+docker compose up --build snapshot-init
