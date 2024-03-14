b65df65a6e7086a41206341cf1d93da9d221675f
Matthew Little <zone117x@gmail.com>
2024-03-14 12:50:42 +0100

    js to ts for tx-broadcaster

--- stacking/tx-broadcaster.ts
+++ stacking/tx-broadcaster.ts
@@ -1,28 +1,37 @@
 import { StacksTestnet } from '@stacks/network';
-import { TransactionVersion, getAddressFromPrivateKey, getNonce, makeSTXTokenTransfer, broadcastTransaction } from '@stacks/transactions';
+import {
+  TransactionVersion,
+  getAddressFromPrivateKey,
+  getNonce,
+  makeSTXTokenTransfer,
+  broadcastTransaction,
+} from '@stacks/transactions';
 
-const broadcastInterval = process.env.BROADCAST_INTERVAL ?? 2;
-const postTxWait = process.env.POST_TX_WAIT ?? 2;
+const broadcastInterval = parseInt(process.env.BROADCAST_INTERVAL ?? '2');
 const url = `http://${process.env.STACKS_CORE_RPC_HOST}:${process.env.STACKS_CORE_RPC_PORT}`;
 const network = new StacksTestnet({ url });
 
-const accounts = process.env.ACCOUNT_KEYS.split(',').map(privKey => ({
+const accounts = process.env.ACCOUNT_KEYS!.split(',').map(privKey => ({
   privKey,
   stxAddress: getAddressFromPrivateKey(privKey, TransactionVersion.Testnet),
 }));
 
 async function run() {
-  const accountNonces = await Promise.all(accounts.map(async (account) => {
-    const nonce = await getNonce(account.stxAddress, network);
-    return { ...account, nonce };
-  }));
+  const accountNonces = await Promise.all(
+    accounts.map(async account => {
+      const nonce = await getNonce(account.stxAddress, network);
+      return { ...account, nonce };
+    })
+  );
 
   // Send from account with lowest nonce
   accountNonces.sort((a, b) => Number(a.nonce) - Number(b.nonce));
   const sender = accountNonces[0];
   const recipient = accountNonces[1];
 
-  console.log(`Sending stx-transfer from ${sender.stxAddress} (nonce=${sender.nonce}) to ${recipient.stxAddress}`);
+  console.log(
+    `Sending stx-transfer from ${sender.stxAddress} (nonce=${sender.nonce}) to ${recipient.stxAddress}`
+  );
 
   const tx = await makeSTXTokenTransfer({
     recipient: recipient.stxAddress,
@@ -31,6 +40,7 @@ async function run() {
     network,
     nonce: sender.nonce,
     fee: 300,
+    anchorMode: 'any',
   });
 
   const broadcastResult = await broadcastTransaction(tx, network);
@@ -39,16 +49,16 @@ async function run() {
   } else {
     console.log(`Broadcast stx-transfer tx=${broadcastResult.txid}`);
   }
-
-  await new Promise(resolve => setTimeout(resolve, postTxWait * 1000));
 }
 
 async function loop() {
-  try {
-    await run();
-  } catch (e) {
-    console.error('Error submitting stx-transfer tx:', e);
+  while (true) {
+    try {
+      await run();
+    } catch (e) {
+      console.error('Error submitting stx-transfer tx:', e);
+    }
+    await new Promise(resolve => setTimeout(resolve, broadcastInterval * 1000));
   }
-  setTimeout(loop, broadcastInterval * 1000);
 }
 loop();
